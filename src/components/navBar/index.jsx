270d88bdd9ec561ea03c79c484dad75f4225e7f1
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-06 18:34:44 +0300

    Add navBar component

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,29 @@
+import "./navBar.css";
+import userProfile from "../../assets/imgs/profile-pic.png";
+
+const NavBar = () => {
+  return (
+    <header className="navBar">
+      <div className="navBarLeft">
+        <i className="navBarIcon fa-brands fa-square-facebook"></i>
+        <i className="navBarIcon fa-brands fa-square-x-twitter"></i>
+        <i className="navBarIcon fa-brands fa-linkedin"></i>
+        <i className="navBarIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="navBarCenter">
+        <ul className="navBarList">
+          <li className="navBarListItem">Home</li>
+          <li className="navBarListItem">About</li>
+          <li className="navBarListItem">Contact</li>
+          <li className="navBarListItem">Write</li>
+        </ul>
+      </div>
+      <div className="navBarRight">
+        <img className="navBarImg" src={userProfile} alt="user profile" />
+        <i className="navBarSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
