7324cff70faf40ae8448ca1ce1b42a720c84438d
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-13 17:10:23 +0300

    Add write and pub;ish functions

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ export const IMAGE_URL = "http://localhost:3300/uploads/";
 
 const api = axios.create({
     baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+    // headers: { "Content-Type": "application/json" },
     withCredentials: true,
 });
 
--- src/pages/Write/index.jsx
+++ src/pages/Write/index.jsx
@@ -1,29 +1,94 @@
 import "./write.css";
+import api from "../../api";
+import { useState } from "react";
 import { post3Img } from "../../assets/imgs";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../context/userContext";
 
 const Write = () => {
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const initialValues = { title: "", desc: "" };
+  const [values, setValues] = useState(initialValues);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleChange = (ev) => {
+    setValues((prevState) => {
+      return { ...prevState, [ev.target.name]: ev.target.value };
+    });
+  };
+
+  const handleFileChange = (ev) => {
+    setSelectedFile(ev.target.files[0]);
+  };
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    setError(null);
+
+    const newPost = {
+      username: user.username,
+      title: values.title,
+      desc: values.desc,
+    };
+
+    const formData = new FormData();
+    const filename = selectedFile?.name;
+    formData.append("title", newPost.title);
+    formData.append("desc", newPost.desc);
+    formData.append("username", newPost.username);
+    formData.append("post", selectedFile);
+    newPost.photo = filename;
+
+    try {
+      const response = await api.post("/posts/", formData);
+      const postId = response.data._id;
+      response.data && navigate(`/post/${postId}`);
+    } catch (error) {
+      if (!error?.response) {
+        setError("No Server Response");
+      } else if (error.response?.data) {
+        setError(error.response?.data.error);
+      }
+    }
+  };
+
   return (
     <div className="write">
       <img className="writeImg" src={post3Img} alt="" />
-      <form className="writeForm">
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fa-solid fa-plus"></i>
           </label>
-          <input id="fileInput" type="file" style={{ display: "none" }} />
+          {<span style={{ color: "red", marginTop: "10px" }}>{error}</span>}
           <input
-            className="writeInput"
-            placeholder="Title"
+            type="file"
+            id="fileInput"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+          <input
+            required
             type="text"
+            name="title"
             autoFocus={true}
+            placeholder="Title"
+            className="writeInput"
+            onChange={handleChange}
           />
         </div>
         <div className="writeFormGroup">
           <textarea
+            required
+            type="text"
+            name="desc"
+            onChange={handleChange}
             className="writeInput writeText"
             placeholder="Tell your story..."
-            type="text"
-            autoFocus={true}
           />
         </div>
         <button className="writeSubmit" type="submit">
