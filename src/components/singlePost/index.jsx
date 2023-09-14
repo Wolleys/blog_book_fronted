4bfa5b6227ea3d07c03c9bb46af8cda45b531f2c
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-09-14 09:24:58 +0300

    Delete a single story by author and post id

--- src/components/singlePost/index.jsx
+++ src/components/singlePost/index.jsx
@@ -2,17 +2,20 @@ import "./singlePost.css";
 import api from "../../api";
 import { IMAGE_URL } from "../../api";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useUser } from "../../context/userContext";
 import { postPlacehoderImg } from "../../assets/imgs";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 const SinglePost = () => {
+  const { user } = useUser();
+  const navigate = useNavigate();
   const postId = useParams().postId;
   const [post, setPost] = useState({});
 
   useEffect(() => {
     const fetchPost = async () => {
       const response = await api.get(`/posts/${postId}`);
-      setPost(response.data);
+      setPost(response?.data);
     };
     fetchPost();
   }, [postId]);
@@ -22,6 +25,18 @@ const SinglePost = () => {
   const fallBackImg = (ev) => {
     ev.onerror = null;
     ev.currentTarget.src = postPlacehoderImg;
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await api.delete(`/posts/${postId}`, {
+        data: { username: user.username },
+      });
+      console.log(response.data);
+      response.data && navigate("/");
+    } catch (error) {
+      console.log(error.response.data);
+    }
   };
 
   return (
@@ -35,10 +50,15 @@ const SinglePost = () => {
         />
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-            <i className="singlePostIcon fa-regular fa-trash-can"></i>
-          </div>
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+              <i
+                className="singlePostIcon fa-regular fa-trash-can"
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span>
